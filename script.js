f937278de927d94c24c0afc6f1d713038e6addc4
Oluwafemi <femikola23@gmail.com>
2021-10-20 19:27:06 +0400

    remove linters error

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ document.addEventListener('DOMContentLoaded', Book.displayBooks);
 
 const form = document.querySelector('#form');
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', () => {
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
   const id = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
