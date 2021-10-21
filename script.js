14ef0facdb28870e9a2db8221a400db5eb834345
Oluwafemi <femikola23@gmail.com>
2021-10-21 16:54:07 +0400

    Update script and styles

--- script.js
+++ script.js
@@ -84,3 +84,35 @@ document.querySelector('#book-list').addEventListener('click', (e) => {
 
   Book.removeBook(e.target.id);
 });
+
+const addNewList = document.getElementById('add-new-book');
+const openForm = document.getElementById('open-form');
+const openContact = document.getElementById('open-contact');
+const addNew = document.getElementById('form-sec');
+const bookList = document.getElementById('add-book');
+const contact = document.getElementById('contact');
+
+function showForm() {
+  addNew.classList.remove('none');
+  bookList.classList.add('none');
+  contact.classList.add('none');
+}
+
+function showBook() {
+  bookList.classList.remove('none');
+  addNew.classList.add('none');
+  contact.classList.add('none');
+}
+
+function showContact() {
+  contact.classList.remove('none');
+  bookList.classList.add('none');
+  addNew.classList.add('none');
+}
+
+openForm.addEventListener('click', showForm);
+
+addNewList.addEventListener('click', showBook);
+
+openContact.addEventListener('click', showContact);
+
